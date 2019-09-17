542a87c4faddb6a881a256e684da9bd899d7711f
JUNHYUK KIM Jay <kim.jh1@gsshop.com>
2019-09-17 18:03:12 +0900

    [add] first day

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import Movie from './Movie';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+//yts.lt에서 제공한 API 사용 예정 https://yts.lt/api/v2/list_movies.json
+//yts proxy라는 API를 별도로 사용예정 https://yts-proxy.now.sh/list_movies.json
+
+class App extends React.Component{      
+
+    //setState를 사용할 때 state안에 default값들을 선언할 필요는 없다.
+    
+    state ={
+        isLoading: true,
+        movies: []
+    };
+
+    getMovies = async() =>{ 
+        const{
+            data:{
+                data:{
+                    movies
+                }
+            }
+        } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+
+        this.setState({movies, isLoading: false});   //this.setState({movies:movies}); 와 같은 뜻
+        
+
+        // 위와 같이 movies.data.data.movies 로 접근하는 것 보다는 위의 방식이 더 나음
+
+        //axios가 다끝날때까지 기다려줘라는 뜻 
+        //async 를 써야 await 이 가능함
+    }
+    componentDidMount(){
+/*        setTimeout(() => {
+
+            this.setState({isLoading: false});
+        },6000);
+  */
+        this.getMovies();
+        //이 과정에서 fetch를 할 예정
+        //fatch 위의 작은 layer임 (일종의 땅콩위의 초콜릿)
+    
+    }
+    render(){
+        const {isLoading, movies} = this.state;
+        return <div> {this.state.isLoading ? "Loading..." 
+        : movies.map(movie => (
+             <Movie
+            key={movie.id}
+            id={movie.id} 
+            year={movie.year} 
+            title={movie.title} 
+            summary={movie.summary} 
+            poster={movie.medium_cover_image}
+            />
+            
+        ))} </div>;
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/*
+// react 는 app.js 컴포넌트를 root 에 넣으려고 하고 있다.
+// react 는 virtual DOM을 중심으로 작동하고 있음
+// <App /> -> 이것을 component 라고 함 HTML을 반환하는 함수 이다.
+// 컴포넌트는 HTML을 반환하는 함수다.
+// HTML과 Javascript 사이에 나온 것이 Jsx ( react에 특화된 개념)
+// javascript 위에서 배우는 것 JSX
+*/
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+ReactDOM.render(<App />,  document.getElementById("potato"));
+
+/*
+//하나의 컴포넌트만 렌더링 가능함 따라서, Potato를 렌더링 하고 싶으면 app.js 조작이 불가피하다. 
+// 서버는 별도로 없기때문에 npm start 하고 그상태로 두어야함.
+*/
