978838cd735bc1710d1c4f758f79dfbbfed7af03
JUNHYUK KIM Jay <kim.jh1@gsshop.com>
2019-09-18 16:09:43 +0900

    Updates

---
+++ precache-manifest.e0b8a868a0f752dbc73b70fd546bda17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90e3cf0fdbf898762469f7a0e6efeb98",
+    "url": "/jays_app_2019/index.html"
+  },
+  {
+    "revision": "28aece7835b70d330197",
+    "url": "/jays_app_2019/static/css/main.5721af45.chunk.css"
+  },
+  {
+    "revision": "65ac2c1af2b258d3dbcc",
+    "url": "/jays_app_2019/static/js/2.551da2b5.chunk.js"
+  },
+  {
+    "revision": "28aece7835b70d330197",
+    "url": "/jays_app_2019/static/js/main.e339962b.chunk.js"
+  },
+  {
+    "revision": "a3a522b2c7293484c3d1",
+    "url": "/jays_app_2019/static/js/runtime~main.cb36b050.js"
+  }
+]);
